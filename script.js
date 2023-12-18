8b363ab01fd9635af4e89f43236ec73da217fbf3
manishapariyar <manishapariyar196@gmail.com>
2023-12-18 15:47:02 +0545

    adding animation

--- script.js
+++ script.js
@@ -23,8 +23,13 @@ window.onscroll = () => {
             links.classList.remove('active');
             document.querySelector('header nav a[href*= '+id+']').classList.add('active');
          })
+         // active sections for animation on scroll
+         sec.classList.add('show-animate');
         }
-     })
+        else{
+         sec.classList.remove('show-animate');
+        }
+     });
     // sticky header
  let header = document.querySelector('.header');
 
@@ -36,4 +41,6 @@ window.onscroll = () => {
     navbar.classList.remove('active');
     // animation .classListfooter on scroll?
 
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animation', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
